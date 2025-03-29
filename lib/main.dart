f5cfef6ba15e4371f96b96ae2eb81e4348d4b817
any-git BST <bosuutap@alwaysdata.net>
2025-03-29 12:47:49 +0000

    feat: Enhance video player functionality and improve error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:developer';
 
 void main() {
   runApp(const MainApp());
@@ -39,23 +39,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   @override
   void initState() {
     super.initState();
-
     _loadVideos();
-  }
-
-  void _loadVideos() async {
-    try {
-      var apiUrl = Uri.parse("https://collection-api.deno.dev/api/files");
-      var response = await http.get(apiUrl);
-      log('API Response: ${response.body}');
-      var data = jsonDecode(response.body);
-      setState(() {
-        _videos = data;
-        _selectedController.text = data[0];
-      });
-    } catch (e) {
-      log('Error loading videos: $e');
-    }
   }
 
   @override
@@ -63,6 +47,60 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     _selectedController.dispose();
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     super.dispose();
+  }
+
+  void _loadVideos() async {
+    try {
+      var apiUrl = Uri.parse("https://collection-api.deno.dev/api/files");
+      var response = await http.get(
+        apiUrl,
+        headers: {"Accept": "application/json"},
+      );
+
+      var data = jsonDecode(response.body);
+      setState(() {
+        data.forEach((video) {
+          _videos.add(video);
+        });
+        loadVideo(_videos[0]);
+      });
+    } catch (e) {
+      showAlertDialog(title: "Error", message: e.toString());
+    }
+  }
+
+  void loadVideo(String videoName) async {
+    var apiUrl = Uri.parse(
+      "https://collection-api.deno.dev/api/presigned-url/$videoName",
+    );
+    var response = await http.get(
+      apiUrl,
+      headers: {"Accept": "application/json"},
+    );
+    var data = jsonDecode(response.body);
+    setState(() {
+      _selectedController.text = data['url'];
+    });
+  }
+
+  void showAlertDialog({String? title, String? message}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title ?? "Alert"),
+          content: Text(message ?? ""),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Đóng dialog
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Widget _buildDrawer() {
@@ -74,9 +112,14 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
           _showDrawer
               ? Column(
                 children: [
-                  Padding(padding: const EdgeInsets.all(8.0)),
-                  const Divider(color: Colors.white30),
-
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _selectedController.text,
+                      style: const TextStyle(color: Colors.white70),
+                    ),
+                  ),
+                  const Divider(color: Colors.white70),
                   Expanded(
                     child: ListView.builder(
                       itemCount: _videos.length,
@@ -84,11 +127,19 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                         return ListTile(
                           title: Text(
                             _videos[index],
-                            style: const TextStyle(color: Colors.white70),
+                            style: TextStyle(
+                              color:
+                                  _selectedController.text == _videos[index]
+                                      ? Color.fromARGB(255, 241, 168, 10)
+                                      : Colors.white70,
+                            ),
                             maxLines: 1,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          onTap: () => log('Selected video: ${_videos[index]}'),
+                          onTap:
+                              () => setState(() {
+                                loadVideo(_videos[index]);
+                              }),
                         );
                       },
                     ),
@@ -99,42 +150,47 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     );
   }
 
+  Widget _buildVideoPlayer() {
+    Uri uri = Uri.parse(_selectedController.text);
+    return Expanded(
+      child: Center(
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: VideoPlayer(VideoPlayerController.networkUrl(uri)),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          _buildDrawer(),
-          Flexible(
-            flex: 1,
-            child: Stack(
-              children: [
-                IconButton(
-                  padding: const EdgeInsets.all(8.0),
-                  icon: Icon(
-                    _showDrawer ? Icons.close : Icons.menu,
-                    color: Colors.white70,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _showDrawer = !_showDrawer;
-                    });
-                  },
-                ),
-                SafeArea(
-                  child: Center(
-                    child: Text(
-                      _selectedController.text,
-                      style: const TextStyle(color: Colors.white70),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+    return SafeArea(
+      child: Scaffold(
+        body: Row(
+          children: [
+            _buildDrawer(),
+            Flexible(
+              flex: 1,
+              child: Stack(
+                children: [
+                  IconButton(
+                    padding: const EdgeInsets.all(8.0),
+                    icon: Icon(
+                      _showDrawer ? Icons.close : Icons.menu,
+                      color: Colors.white70,
                     ),
+                    onPressed: () {
+                      setState(() {
+                        _showDrawer = !_showDrawer;
+                      });
+                    },
                   ),
-                ),
-              ],
+                  Center(child: _buildVideoPlayer()),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
