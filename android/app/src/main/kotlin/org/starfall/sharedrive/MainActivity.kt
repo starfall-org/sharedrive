9c1fab81b4383e66f78e7c23a27bc15fdaa705b6
Org Contributor <tonglanhchua@gmail.com>
2025-04-24 09:01:20 +0000

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and compatibility with Windows 10 and 11.
    - Implemented utility functions for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

---
+++ android/app/src/main/kotlin/org/starfall/sharedrive/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.starfall.sharedrive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import dynamic_color
+import file_picker
+import package_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+}
